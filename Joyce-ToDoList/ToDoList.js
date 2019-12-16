1b7dc76dde0cbb309af633d7782682c4f5a857b0
JoyceQu <qulecheng2004@163.com>
2019-12-16 20:42:08 +0800

    updated the todolist by using vue
    -rewrite the todolist by using vue.js framework
    -add the sentence function to display number of todo and done items

--- Joyce-ToDoList/ToDoList.js
+++ Joyce-ToDoList/ToDoList.js
@@ -1,34 +1,105 @@
-
-function appendTodo()
-{
-    document.getElementById('content').focus();
-    var text = document.getElementById("content").value;
-    //如果输入为空弹出提示
-    if (text == "")
+var vm = new Vue(
     {
-        alert("Please write something!");
-    }
-    else
+    el: '#sth',    
+    data: 
     {
-        document.getElementById("content").value = "";
-        //列表里新建li
-        var list = document.getElementById("list");
-        var li = document.createElement("li");
-        list.appendChild(li);
-        //li里加入label，为输入框里的内容
-        var label = document.createElement("label");
-        label.appendChild(document.createTextNode(text));
-        li.appendChild(label);
-        //li里加入button，用于delete
-        var button = document.createElement("button");
-        button.appendChild(document.createTextNode("Delete"));
-        button.className = "delbtn";
-        li.appendChild(button);
-        //点击delete button删除当前li
-        button.onclick = function()
+        ToDoList : [],      //list of items
+        ToDoThing : '',     //input
+        DoneNum : 0,        //number of todo items
+        TodoNum : 0,        //number of done items
+        TodoSent : 'You dont have anything to do! Enjoy!',
+        DoneSent : 'Nothing is done. Work hard!',
+        todos : '',         //any "s" after thing?
+        dones : ''          
+    },
+    methods: 
+    {
+        Calsent: function(){
+        //to derive the sentence outputs
+            if (this.TodoNum <= 1)
+            {
+                this.todos = '';
+            }
+            else
+            {
+                this.todos = 's';
+            }
+            if (this.DoneNum <= 1)
+            {
+                this.dones = '';
+            }
+            else
+            {
+                this.dones = 's';
+            }
+            if (this.TodoNum <= 0)
+            {
+                this.TodoSent = "You don't have anything to do! Enjoy!";
+            }
+            else
+            {
+                this.TodoSent = 'You have ' + this.TodoNum + " thing" + this.todos + " todo.";
+            }
+            if (this.DoneNum <= 0)
+            {
+                this.DoneSent = 'Nothing is done. Work hard!';
+            }
+            else
+            {
+                this.DoneSent = 'You have done ' + this.DoneNum + " thing" + this.dones + ". Keep going!";
+            }
+        },
+        AppendToDo: function()
+        //append a todo item
         {
-            list.removeChild(li);
-            document.getElementById('content').focus();
+            if (this.ToDoThing === '')
+            //warning for not entering things
+            {
+                alert("Please write something!");
+                return;
+            }
+            else
+            {
+                this.TodoNum += 1;
+                this.Calsent();
+                this.ToDoList.push({
+                    content : this.ToDoThing,
+                    IsDone : false
+                });
+                this.ToDoThing = '';
+                this.$refs.content.focus();
+            }
+        },
+
+        DeleteToDo: function(item)
+        //delete a todo item
+        {
+            this.TodoNum -= 1;
+            this.Calsent();
+            var index = this.ToDoList.indexOf(item);
+            this.ToDoList.splice(index, 1);
+            this.$refs.content.focus();
+        },
+
+        ClearAll: function()
+        //clear the todo list
+        {
+            this.ToDoList = [];
+            this.DoneNum = 0;
+            this.TodoNum = 0;
+            this.Calsent();
+            this.$refs.content.focus();
+        },
+
+        ChangeState: function(item)
+        //determine if the item is done or not
+        {
+            item.IsDone = 1 - item.IsDone;
+            this.DoneNum += item.IsDone * 2 - 1
+            this.TodoNum -= 1;
+            this.Calsent();
+            this.$refs.content.focus();
         }
-    }   
-}
+    }
+});
+
